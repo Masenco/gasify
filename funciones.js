5ac7ca2a144ab9fc4c3145b9bc7cadedb2b79224
Tomás Atampi <tomiatampi@gmail.com>
2021-03-02 07:07:21 -0300

    Responsive design agregado

--- funciones.js
+++ funciones.js
@@ -1,14 +1,24 @@
 function openMenu() {
     const toggle_menu = document.querySelector('.open_menu')
     toggle_menu.classList.toggle("active");
+
+
     const toggle_menu2 = document.querySelector('.menu_toggle')
     toggle_menu2.classList.toggle("active");
+
+
     const open_menu = document.querySelector('.content_navegation');
     open_menu.classList.toggle("active");
+
+
     const open_menu2 = document.querySelector('.links-sections_right');
     open_menu2.classList.toggle("active");
+
+
     const open_menu3 = document.querySelector('.links-sections_left');
     open_menu3.classList.toggle("active");
+
+    
     const logo = document.querySelector('.logo');
     logo.classList.toggle("active");
     
