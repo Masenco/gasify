6930bc39f57947d41ae9759b1eefe5ca43b30185
Tomás Atampi <tomiatampi@gmail.com>
2021-03-03 07:57:26 -0300

    Agregado de animaciones en scroll

--- funciones.js
+++ funciones.js
@@ -25,6 +25,29 @@ function openMenu() {
 }
 
 function closeMenu() {
-    const open_menu = document.querySelector('.links_sections');
-    open_menu.classList.remove("active");
+    const close_menu = document.querySelector('.content_navegation');
+    close_menu.classList.remove("active");
+
+    const closeToggle = document.querySelector('.open_menu')
+    closeToggle.classList.remove("active");
+
+
+    const closeToggle2 = document.querySelector('.menu_toggle')
+    closeToggle2.classList.remove("active");
+
+
+    const close_menu2 = document.querySelector('.content_navegation');
+   close_menu2.classList.remove("active");
+
+
+    const close_menu3 = document.querySelector('.links-sections_right');
+    close_menu3.classList.remove("active");
+
+
+    const close_menu4 = document.querySelector('.links-sections_left');
+   close_menu4.classList.remove("active");
+
+    
+    const logo_desactive = document.querySelector('.logo');
+    logo_desactive.classList.remove("active");
 }
